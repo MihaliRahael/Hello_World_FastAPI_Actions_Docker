c56b5531029992a110302e750dfd9820d9ebc340
Bonny Philip <106816732+MihaliRahael@users.noreply.github.com>
2022-10-19 20:05:37 +0530

    Files added

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.7
+COPY . /app
+WORKDIR /app
+RUN pip3 install -r requirements.txt
+EXPOSE 8000
+CMD ["uvicorn", "app:app", "--host=0.0.0.0", "--reload"]
---
+++ app.py
@@ -0,0 +1,18 @@
+# Library imports
+import uvicorn
+from fastapi import FastAPI
+
+# Create the app object
+app = FastAPI()
+
+# Index route, opens automatically on http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
+
+#    Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000, reload=True)
+    
+#uvicorn app:app --reload
